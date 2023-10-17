67bca60044c0e874a50b6a07e6838a3ff9a2b493
Henrique Izidio <izidiohenrique78@gmail.com>
2023-10-17 14:03:27 +0000

    intriga

---
+++ src/intriga.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#include "./tool_box.h"
+
+
+controle = casa1
+casa1 = casa2
+casa2 = controle
+
+void intriga(){
+    
+}

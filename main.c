dc2cd53bcad5aee81e59ab370a92eb0f0b04e912
Henrique Izidio <izidiohenrique78@gmail.com>
2023-10-28 22:40:38 +0000

    tutorial

--- main.c
+++ main.c
@@ -12,7 +12,6 @@
 
 int main() {
     void Menu();
-    void introduc();
     int Dificuldade();
     void clearScreen();
     void setAnsi();
@@ -37,39 +36,47 @@ int main() {
                 break;
             case 2:
                 clearScreen();
-                printf(COR_VERDE "\nExibindo os créditos...\n"RESET_COR);
-                printf(COR_MAGENTA"\n JOGO FEITO PELO PROFESSOR HUGO ROBERTO PINHEIRO, \n ASSIM COMO O TRABALHO PARA A TURMA 2023.1 DE CIÊNCIDA DA COMPUTAÇÃO"RESET_COR);
-                printf(COR_MAGENTA"\n QUEM ESCREVEU O CÓDIGO.....HENRIQUE IZÍDIO, ISAIAS FERNANDES E PHELIPPE \n \n "RESET_COR);
+                printf(COR_VERDE "\nExibindo os creditos...\n"RESET_COR);
+                printf(COR_MAGENTA"\n JOGO FEITO PELO PROFESSOR HUGO ROBERTO PINHEIRO, \n ASSIM COMO O TRABALHO PARA A TURMA 2023.1 DE CIENCIDA DA COMPUTAÇAO"RESET_COR);
+                printf(COR_MAGENTA"\n QUEM ESCREVEU O CODIGO.....HENRIQUE IZIDIO, ISAIAS FERNANDES E PHELIPPE \n \n "RESET_COR);
                 getchar();
                 clearScreen();
                 break;
             case 3:
                 clearScreen();
-                printf(COR_CYAN  "\nSaindo do jogo. Até logo!\n"RESET_COR);
+                printf("COMANDOS DO JOGO\n");
+                printf("Fase de Expanção\n");
+                printf("1 - Para posicionar uma peça no tabuleiro digite a coordernada onde quer inseri-la\n");
+                printf("    - Exemplo: \"A1\", sem aspas.");
+                printf("2 - E possivel mover o tabuleiro para deixa-lo da melhor forma possivel. Para isso use os seguintos comandos\n");
+                printf("    - MVD: Move para a direita\n");
+                printf("    - MVE: Move para a esquerda\n");
+                printf("    - MVC: Move para a cima\n");
+                printf("    - MVB: Move para a baixo\n");
+                printf("Apenas é possivel mover o tabuleiro comleto. Só é possivel mover para determinada direção caso a borda da mesma esteja desocupada.\n\n");
+                
+                printf("Fase de Intriga:\n");
+                printf("1 - Para realizar a troca de duas peças digite as coordenadas de ambas as peças uma seguida da outra.\n");
+                printf("    - Exemplo: \"A1B2\", sem aspas.\n\n");
+
+                printf("Durante ambas as fases é possivel retornar ao menu com o comando \"SAIR\", sem aspas.\n");
+                printf("E indiferente escrever os comandos com letras maiusculas ou minusculas\n");
+                printf("Porem as coordenadas devem ser escritas com a letra primeiro e em seguida o numero.\n");
+
+                getchar();
+
+                break;
+            case 4:
+                clearScreen();
+                printf(COR_CYAN  "\nSaindo do jogo. Ate logo!\n"RESET_COR);
                 return 0;
             default:
-                printf("\nOpção inválida. Por favor, escolha uma opção válida.\n");
+                printf("\nOpção invalida. Por favor, escolha uma opcao valida.\n");
                 break;
         }
     }
 
     return 0;
-}
-
-void introduc() {
-    printf(COR_AMARELA "\n \t \t \t AS CASAS ESTÃO DISPUTANDO FEROZMENTE PELO TRONO\n " RESET_COR);
-    getchar();
-    printf(COR_AZUL "\t \t \t RESTA VOCÊ, DONO DE TODOS OS SEGREDOS, MANIPULAR VASSALOS E NOBRES PARA CHEGAR A UM RESULTADO QUE, APESAR DE NÃO AGRADAR A TODOS, TRARÁ PAZ\n" RESET_COR);
-    getchar();
-    printf(COR_VERMELHA "\t \t \t ENTRETANTO, O TEMPO É ESCASSO E ALGUMAS DAS SUAS PRECIOSAS INFORMAÇÕES TARDAM A CHEGAR.\n" RESET_COR);
-    getchar();
-    printf(COR_VERDE "\t \t \t VOCÊ PRECISARÁ TRABALHAR COM INCERTEZAS POR UM MOMENTO.\n" RESET_COR);
-    getchar();
-    printf(COR_MAGENTA "\t \t \t E QUANDO AS INFORMAÇÕES CHEGAREM... TALVEZ SEJA TARDE DEMAIS PARA REVERTER A SITUAÇÃO.\n" RESET_COR);
-    getchar();
-    printf(COR_CYAN "\t \t \t A GUERRA PODE ESTAR MAIS PERTO DO QUE NUNCA E A ÚNICA PESSOA CAPAZ DE EVITÁ-LA É VOCÊ.\n" RESET_COR);
-    getchar();
-    clearScreen();
 }
 
 void Menu() {
@@ -81,12 +88,13 @@ void Menu() {
     printf("******** REGICIDA ********\n");
     printf("\x1b[0m");
     printf(COR_AMARELA "1. Jogar\n"RESET_COR);
-    printf(COR_VERDE "2. Créditos\n"RESET_COR);
-    printf(COR_MAGENTA "3. Sair\n"RESET_COR);
+    printf(COR_VERDE "2. Creditos\n"RESET_COR);
+    printf(COR_VERMELHA "3. Tutorial\n"RESET_COR);
+    printf(COR_MAGENTA "4. Sair\n"RESET_COR);
     printf("\x1b[31m");
     printf("*******************\n");
     printf("\x1b[0m");
-    printf("Escolha uma opção: ");
+    printf("Escolha uma opcao: ");
 }
 
 int Dificuldade(){
